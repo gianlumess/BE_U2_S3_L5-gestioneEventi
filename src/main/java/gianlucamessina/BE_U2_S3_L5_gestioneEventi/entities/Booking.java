d729c40419aa32d905f9d2b6296ae4319d36d18d
Gianluca Messina <gianluca.messina8201@gmail.com>
2024-09-20 17:00:16 +0200

    creata entity Booking

---
+++ src/main/java/gianlucamessina/BE_U2_S3_L5_gestioneEventi/entities/Booking.java
@@ -0,0 +1,33 @@
+package gianlucamessina.BE_U2_S3_L5_gestioneEventi.entities;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.*;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class Booking {
+    @Id
+    @GeneratedValue
+    @Setter(AccessLevel.NONE)
+    private UUID id;
+    private int seatsBooked;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+
+    public Booking(int seatsBooked, User user, Event event) {
+        this.seatsBooked = seatsBooked;
+        this.user = user;
+        this.event = event;
+    }
+}
